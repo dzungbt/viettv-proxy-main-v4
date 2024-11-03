7f490c4786026cce821a14582cf98eeff5d0ad6c
dungbt-dev <thedung.1292@gmail.com>
2024-11-03 09:36:07 +0700

    update log

--- hls-proxy/bin/hlsd.js
+++ hls-proxy/bin/hlsd.js
@@ -115,8 +115,9 @@ if (middleware.request){
           return;
         }
         let userData = await auth(username, password);
-        if (userData && !username == 'zBUV') {
+        if (userData && username != 'zBUV') {
           // http://103.72.97.184:5005/video/service_on_maintain.ts
+          console.log('user reject cause on maintain : ', username)
           req.url = '/aHR0cDovLzEwMy43Mi45Ny4xODQ6NTAwNS92aWRlby9zZXJ2aWNlX29uX21haW50YWluLnRz'
           middleware.request(req, res);
           return;
